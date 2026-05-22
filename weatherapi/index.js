ae8055163c227b00a680a2ec132436725ba0eaf6
Ratnesh Kumar <rk02ratnesh@gmail.com>
2026-05-22 15:48:36 +0530

    weatherapi completed

--- weatherapi/index.js
+++ weatherapi/index.js
@@ -1,8 +1,19 @@
 let express = require('express');
 let request = require('request');
+let ejs = require('ejs');
 
 let app = express();
 let port = process.env.PORT || 8000;
+
+
+//static api
+app.use(express.static(__dirname + '/public'));
+
+//html file
+app.set('views', './src/views');
+
+//view engine
+app.set('view engine', 'ejs');
 
 //default route
 app.get('/', (req, res) => {
@@ -14,10 +25,13 @@ app.get('/weather', (req, res) => {
 
     let url = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`;
 
+
     //calling api
     request(url, (err, response) => {
         if (err) throw err;
-        res.send(response.body);
+        let result = JSON.parse(response.body);
+        let city = result["city"];
+        res.render('index', { title: 'Weather App', result });
     });
     
 

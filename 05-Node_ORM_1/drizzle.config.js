9bb8d7bbe42062a02fa217e2b7d7795952c78e2e
Ratnesh Kumar <rk02ratnesh@gmail.com>
2026-06-13 22:52:43 +0530

    orm-1 updated

---
+++ 05-Node_ORM_1/drizzle.config.js
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+    
+	dialect: 'postgresql',
+	out: './drizzle',
+	schema: './drizzle/schema.js',
+	dbCredentials: {
+		url: process.env.DATABASE_URL,
+	},
+});
---
+++ 05-Node_ORM_1/drizzle/schema.js
@@ -0,0 +1,10 @@
+import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+
+const userTable = pgTable("users", {
+    id: integer().primaryKey(),
+    name: varchar({ length: 255 }).notNull(),
+    email : varchar({length:255}).notNull().unique()
+});
+
+
+export { userTable };

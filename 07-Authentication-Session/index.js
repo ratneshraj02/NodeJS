1203ba6de50fed7ee5c74ed1a27ae316ed7bae38
Ratnesh Kumar <rk02ratnesh@gmail.com>
2026-06-16 02:23:17 +0530

    Authentication-session updated

--- 07-Authentication-Session/index.js
+++ 07-Authentication-Session/index.js
@@ -1,24 +1,48 @@
 import express from 'express';
 import userRouter from './routes/user.route.js';
-
+import db from './db/index.js';
+import { usersTable, userSessions } from './db/schema.js';
+import { eq } from 'drizzle-orm';
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-
 //middleware
 app.use(express.json());
 
+app.use('/', async (req, res, next) => {
+	const sessionId = req.headers['session-id'];
+
+	if (!sessionId) {
+		return next();
+	}
+
+	const [data] = await db
+		.select({
+            sessionId: userSessions.id,
+            id: usersTable.id,
+			userId: userSessions.userId,
+			name: usersTable.name,
+			email: usersTable.email,
+		})
+		.from(userSessions)
+		.rightJoin(usersTable, eq(usersTable.id, userSessions.userId))
+		.where((table) => eq(table.sessionId, sessionId));
+
+	if (!data) {
+		return next();
+	}
+
+	req.user = data;
+	next();
+});
 
 app.get('/', (req, res) => {
-    res.send("Sever is running");
+	res.send('Sever is running');
 });
 
 app.use('/user', userRouter);
 
-
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+	console.log(`Server is listening on port ${port}`);
 });
-
-
--- 07-Authentication-Session/routes/user.route.js
+++ 07-Authentication-Session/routes/user.route.js
@@ -1,16 +1,37 @@
 import express from 'express';
 import db from '../db/index.js';
-import {usersTable, userSessions} from '../db/schema.js';
+import { usersTable, userSessions } from '../db/schema.js';
 import { eq } from 'drizzle-orm';
 import { randomBytes, createHmac } from 'node:crypto';
 
 const router = express.Router();
 
+router.patch('/', async function (req, res) {
+    const user = req.user;
 
+    if (!user) {
+        res.status(401).json({ error: 'You are not logged in' });
+    }
+
+    const { name } = req.body;
+    await db.update(usersTable).set({ name }).where(eq(usersTable.id, user.id));
+
+    return res.status(201).json({status : "success"});
+});
+
+router.get('/', async function (req, res) {
+
+    const user = req.user;
+
+	if (!user) {
+		res.status(401).json({ error: 'You are not logged in' });
+	}
+    
+	return res.json({ user  });
+});
 
 router.post('/signup', async function (req, res) {
-
-    const { name, email, password } = req.body;
+	const { name, email, password } = req.body;
 
 	const [existingUser] = await db
 		.select({
@@ -37,46 +58,42 @@ router.post('/signup', async function (req, res) {
 			salt,
 		})
 		.returning({ id: usersTable.id });
- 
+
 	return res.status(201).json({ status: 'success', data: { userId: user.id } });
-}); 
+});
 router.post('/login', async function (req, res) {
+	const { email, password } = req.body;
 
-    const { email, password } = req.body;
+	const [existingUser] = await db
+		.select({
+			id: usersTable.id,
+			email: usersTable.email,
+			salt: usersTable.salt,
+			password: usersTable.password,
+		})
+		.from(usersTable)
+		.where((table) => eq(table.email, email));
 
-    const [existingUser] = await db
-        .select({
-                id: usersTable.id,
-                email: usersTable.email,
-                salt: usersTable.salt,
-                password : usersTable.password
-			})
-			.from(usersTable)
-			.where((table) => eq(table.email, email));
+	if (!existingUser) {
+		res.status(404).json({ error: `user with email ${email} doesn't exist` });
+	}
 
-		if (!existingUser) {
-			res.status(404).json({ error: `user with email ${email} doesn't exist` });
-        }
-    
-    const salt = existingUser.salt;
-    const existingHash = existingUser.password  ;
+	const salt = existingUser.salt;
+	const existingHash = existingUser.password;
 
-    const newHash = createHmac('sha256', salt)
-		.update(password)
-        .digest('hex'); 
-    
-     
-    if (newHash != existingHash) {
-        res.status(400).json({ error: "incorrect password" });
-        
-    }
+	const newHash = createHmac('sha256', salt).update(password).digest('hex');
 
-    const [session] = await db.insert(userSessions).values({
-        userId: existingUser.id
-    }).returning({id : userSessions.id});
-    return res.json({status: "success", sessionId : session.id});
+	if (newHash != existingHash) {
+		res.status(400).json({ error: 'incorrect password' });
+	}
 
-
+	const [session] = await db
+		.insert(userSessions)
+		.values({
+			userId: existingUser.id,
+		})
+		.returning({ id: userSessions.id });
+	return res.json({ status: 'success', sessionId: session.id });
 });
 
 export default router;

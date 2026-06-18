14e94fcb779fc2c60088487030852b3e938f253c
Ratnesh Kumar <rk02ratnesh@gmail.com>
2026-06-18 18:47:11 +0530

    authentication jwt updated

--- 08-Authentication-JWT/controller/user.controller.js
+++ 08-Authentication-JWT/controller/user.controller.js
@@ -1,13 +1,19 @@
 import db from '../db/index.js';
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
+import { userTable } from '../db/schema.js';
+import { eq } from 'drizzle-orm';
+import { randomBytes, createHmac } from 'node:crypto';
+import 'dotenv/config';
+
 
 async function signupController(req, res) {
+	console.log(process.env.JWT_SECRET);
 	const { name, email, password } = req.body;
 	const [existingUser] = await db
 		.select({
-			email: usersTable.email,
+			email: userTable.email,
 		})
-		.from(usersTable)
+		.from(userTable)
 		.where((table) => eq(table.email, email));
 
 	if (existingUser) {
@@ -19,16 +25,18 @@ async function signupController(req, res) {
 		.digest('hex');
 
 	const [user] = await db
-		.insert(usersTable)
+		.insert(userTable)
 		.values({
 			name,
 			email,
 			password: hashPassword,
 			salt,
 		})
-		.returning({ id: usersTable.id });
+		.returning({ id: userTable.id });
 
-	return res.status(201).json({ status: 'success', data: { userId: user.id } });
+	return res
+		.status(201)
+		.json({ status: 'success', data: { userId: user.id} });
 }
 
 async function loginController(req, res) {
@@ -36,12 +44,12 @@ async function loginController(req, res) {
 
 	const [existingUser] = await db
 		.select({
-			id: usersTable.id,
-			email: usersTable.email,
-			salt: usersTable.salt,
-			password: usersTable.password,
+			id: userTable.id,
+			email: userTable.email,
+			salt: userTable.salt,
+			password: userTable.password,
 		})
-		.from(usersTable)
+		.from(userTable)
 		.where((table) => eq(table.email, email));
 
 	if (!existingUser) {
@@ -59,13 +67,42 @@ async function loginController(req, res) {
 
 	const payload = {
 		id: existingUser.id,
-		name : existingUser.name,
+		name: existingUser.name,
 		email: existingUser.email,
-	}
+	};
 
 	const token = jwt.sign(payload, process.env.JWT_SECRET);
+	console.log(token);
 
 	return res.json({ status: 'success', token: token });
 }
 
-export { loginController, signupController };
+async function updateController(req, res) {
+	const user = req.user;
+
+	if (!user) {
+		res.status(401).json({ error: 'You are not logged in' });
+	}
+
+	const { name } = req.body;
+	await db.update(userTable, set({ name })).where(eq(userTable.id, user.id));
+
+	return res.status(201).json({ status: 'success' });
+}
+
+async function isLoginController(req, res) {
+	const user = req.user;
+
+	if (!user) {
+		res.status(401).json({ error: 'You are not logged in' });
+	}
+
+	return res.json({ user });
+}
+
+export {
+	loginController,
+	signupController,
+	updateController,
+	isLoginController,
+};
--- 08-Authentication-JWT/routes/user.router.js
+++ 08-Authentication-JWT/routes/user.router.js
@@ -1,19 +1,21 @@
 import express from 'express';
 import db from '../db/index.js';
-import { loginController, signupController } from '../controller/user.controller.js';
+import {
+	isLoginController,
+	loginController,
+	signupController,
+	updateController,
+} from '../controller/user.controller.js';
 import { eq } from 'drizzle-orm';
 import { randomBytes, createHmac } from 'node:crypto';
-
+import { userTable } from '../db/schema.js';
 
 const router = express.Router();
 
+router.patch('/', updateController);
 
-
-router.get('/', userController);
-
-
-router.post('/login', loginController)
+router.get('/', isLoginController);
+router.post('/login', loginController);
 router.post('/signup', signupController);
-
 
 export default router;

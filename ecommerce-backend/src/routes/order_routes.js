033203d75ebb68d38952d1f015d19451568d8597
Ratnesh Kumar <rk02ratnesh@gmail.com>
2026-08-06 17:14:56 +0530

    remove ecommerce backend

--- ecommerce-backend/src/routes/order_routes.js
+++
@@ -1,8 +0,0 @@
-const OrderRoutes = require('express').Router();
-const OrderController = require('./../controllers/order_controller');
-
-OrderRoutes.get("/:userId", OrderController.fetchOrdersForUser);
-OrderRoutes.post("/", OrderController.createOrder);
-OrderRoutes.put("/updateStatus", OrderController.updateOrderStatus);
-
-module.exports = OrderRoutes;
--- ecommerce-backend/src/routes/user_routes.js
+++
@@ -1,8 +0,0 @@
-const UserRoutes = require('express').Router();
-const UserController = require('./../controllers/user_controller');
-
-UserRoutes.post("/createAccount", UserController.createAccount);
-UserRoutes.post("/signIn", UserController.signIn);
-UserRoutes.put("/:id", UserController.updateUser);
-
-module.exports = UserRoutes;
